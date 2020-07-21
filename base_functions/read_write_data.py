1c7fd30727824bdeb0d88b23a95fa0e94c70cdc7
abinj <abinmj656@gmail.com>
2020-07-21 16:10:29 -0400

    read and write file with spark done successfully

---
+++ base_functions/read_write_data.py
@@ -0,0 +1,31 @@
+from pyspark.sql import SparkSession
+
+spark = SparkSession \
+    .builder \
+    .appName("read_write_data") \
+    .getOrCreate()
+
+print(spark.sparkContext.getConf().getAll())
+path = "/home/abin/my_works/github_works/spark-learning/dataset/sparkify_log_small.json";
+user_log = spark.read.json(path)
+
+user_log.printSchema()
+
+user_log.describe()
+
+user_log.show(n=1)
+
+user_log.take(5)
+
+out_path = "/home/abin/my_works/github_works/spark-learning/dataset/sparkify_log_small.csv"
+
+user_log.write.save(out_path, format='csv', header=True)
+user_log_2 = spark.read.csv(out_path, header=True)
+
+user_log_2.printSchema
+
+user_log_2.take(2)
+
+user_log_2.select("userID").show()
+
+user_log_2.take(1)
