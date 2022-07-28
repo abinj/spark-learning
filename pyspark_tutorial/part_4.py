562238d496c81e792cd3bc2316840710f5bc9d07
Abin <abinmj656@gmail.com>
2022-07-28 15:52:57 -0400

    Groupby operations learned

--- pyspark_tutorial/part_4.py
+++ pyspark_tutorial/part_4.py
@@ -4,17 +4,26 @@ from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.appName("part_4").getOrCreate()
 
-df_pyspark = spark.read.csv("../dataset/test3.csv", header=True, inferSchema=True)
-df_pyspark.printSchema()
+# df_pyspark = spark.read.csv("../dataset/test3.csv", header=True, inferSchema=True)
+# df_pyspark.printSchema()
+#
+# df_pyspark.groupBy('Name').sum().show()
+#
+#
+# df_pyspark.groupBy("Departments").mean().show()
+#
+# df_pyspark.groupBy("Departments").max().show()
+#
+# df_pyspark.groupBy("Departments").min().show()
+#
+# df_pyspark.groupBy("Name").avg().show()
 
-df_pyspark.groupBy('Name').sum().show()
 
 
-df_pyspark.groupBy("Departments").mean().show()
 
-df_pyspark.groupBy("Departments").max().show()
-
-df_pyspark.groupBy("Departments").min().show()
-
-df_pyspark.groupBy("Name").avg().show()
+rdd = spark.sparkContext.parallelize((0,1,2,3,4,5,6,7))
+print(rdd.collect())
+print(rdd.getNumPartitions())
+rdd1 = rdd.coalesce(2)
+print(rdd1.getNumPartitions())
 
